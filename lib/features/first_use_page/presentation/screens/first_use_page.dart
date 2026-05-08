a4a970cc99fe978721c050387d070ac9bf9ab8d5
ibrahimsohaill <143931947+suhel-abrahem@users.noreply.github.com>
2026-05-08 07:43:50 +0300

    Rebrand app: update name, icons and splash assets
    Change Android application label to "Salihly" and add round icon. Replace and add adaptive launcher icons, ic_launcher_foreground images, mipmap assets and colors.xml. Update splash images for Android, iOS and web, add new asset icons (logo and dark variants), and refresh iOS AppIcon and LaunchImage assets. Remove flutter_launcher_icons.yaml and update pubspec, asset manager and a few UI pages to use the new branding.

--- lib/features/first_use_page/presentation/screens/first_use_page.dart
+++ lib/features/first_use_page/presentation/screens/first_use_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hosta_provider/core/extensions/theme_data_extensions.dart';
 import '../../../../config/app/app_preferences.dart';
 import '../../../../config/route/routes_manager.dart';
 import '../../../../config/theme/app_theme.dart';
@@ -52,12 +53,17 @@ class _FirstUsePageState extends State<FirstUsePage> {
       child: ListView(
         children: [
           Padding(
-            padding: EdgeInsetsGeometry.symmetric(vertical: 50.h),
+            padding: EdgeInsetsGeometry.only(top: 30.h),
             child: SvgPicture.asset(
               getAssetsPath(
-                assetsName: ImagesName.loginImage,
-                assetsType: AssetsType.image,
+                assetsName: Theme.of(context).isDark
+                    ? IconsName.darkLogo
+                    : IconsName.logo,
+                assetsType: AssetsType.icon,
               ),
+              width: 100.w,
+              height: 200.h,
+              fit: BoxFit.cover,
             ),
           ),
           Padding(
